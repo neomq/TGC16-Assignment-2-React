781ae61c6813cdd66f8d32935ca18b0b0bfb57ea
neomq <neo.minqi@trentglobal.edu.sg>
2022-04-03 14:32:35 +0000

    another commit

--- home-diy-ideas/src/App.js
+++ home-diy-ideas/src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './App.css'
 import axios from "axios"
 import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap/dist/js/bootstrap.bundle.js"
+import "bootstrap/dist/js/bootstrap.bundle"
 import BrowseProject from './components/BrowseProject.js'
 import SearchResults from './components/SearchResults.js'
 import SearchProject from './components/SearchProject.js'
