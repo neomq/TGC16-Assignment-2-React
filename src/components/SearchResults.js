edf76e13b86786abd3fefa4ccd7202e49bb82756
neomq <neo.minqi@trentglobal.edu.sg>
2022-06-01 12:50:40 +0000

    bug fix + styling adjustments

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -162,9 +162,9 @@ export default function SearchResults (props) {
                                     onChange={props.updateFormField}>
                                     <option value="">Category</option>
                                     {props.category_list.map((c) =>
-                                        <option key={c.category}
-                                            value={c.category}
-                                        >{c.form_display}
+                                        <option key={c.id}
+                                            value={c.value}
+                                        >{c.label}
                                         </option>
                                     )}
                                 </select>
